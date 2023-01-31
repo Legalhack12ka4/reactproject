a30f9ee9848b0a3cf1e58527ff78ca7311184468
Legalhack12ka4 <kumharv0000@gmail.com>
2023-01-31 18:01:27 +0530

    page not found

---
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './PageNotFound.scss';
+
+function PageNotFound() {
+  return (
+    <div className='not-found'>
+     
+    <h1 className="display-1">
+        404
+        Page Not Found
+    </h1> 
+
+  </div>
+  )
+}
+
+export default PageNotFound
