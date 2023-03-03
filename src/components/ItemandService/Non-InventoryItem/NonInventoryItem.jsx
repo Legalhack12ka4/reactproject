145441b528617e23af5eabe6c4fe3fa771a9b17b
ranveersinghreformiqo <ranveer.singh@reformiqo.com>
2023-03-03 14:21:43 +0530

    tags field added

--- src/components/ItemandService/Non-InventoryItem/NonInventoryItem.jsx
+++ src/components/ItemandService/Non-InventoryItem/NonInventoryItem.jsx
@@ -6,6 +6,7 @@ import { PlusOutlined } from "@ant-design/icons";
 
 import SearchDropdown from "../../AllDropdowns/SearchDropdown/SearchDropdown";
 import Page_heading from "../../Page_Heading/Page_heading";
+import TagsInput from "../../TagsInput/TagsInput";
 
 const getBase64 = (file) => {
   return new Promise((resolve, reject) => {
@@ -228,8 +229,8 @@ const NewInventoryItem = () => {
 
             <div className="input_group">
               <p>Tags</p>
-              <div className="input_container focus-outline">
-                <input type="text" placeholder="Placeholder" />
+              <div className="tags_input_container focus-outline">
+                <TagsInput />
               </div>
             </div>
           </div>
