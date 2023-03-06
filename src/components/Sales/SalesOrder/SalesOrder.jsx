df025613cf41a7337254274e2264fa4f49a8d270
ranveersinghreformiqo <ranveer.singh@reformiqo.com>
2023-03-06 18:04:21 +0530

    sales order form changes

--- src/components/Sales/SalesOrder/SalesOrder.jsx
+++ src/components/Sales/SalesOrder/SalesOrder.jsx
@@ -2,11 +2,25 @@ import React, { useState } from "react";
 import Page_heading from "../../Page_Heading/Page_heading";
 import SearchDropdown from "../../AllDropdowns/SearchDropdown/SearchDropdown";
 import "./SalesOrder.scss";
+import { Button, Upload } from "antd";
+import { UploadOutlined } from "@ant-design/icons";
 
 const SalesOrder = () => {
   const [salesOrderItemList, setSalesOrderItemList] = useState([
     { id: 1, name: "row1", value: "" },
   ]);
+  const [fileList, setFileList] = useState([]);
+
+  const handleAddItemRow = () => {
+    setSalesOrderItemList([
+      ...salesOrderItemList,
+      { id: salesOrderItemList.length + 1, name: "row1", value: "" },
+    ]);
+  };
+  const onChange = ({ fileList: newFileList }) => {
+    setFileList(newFileList);
+  };
+
   return (
     <div className="sales-order-main-container">
       <Page_heading parent={"Transactions"} child={"Sales Order"} />
@@ -72,29 +86,89 @@ const SalesOrder = () => {
 
           <div className="sales-order-items-row-container">
             {salesOrderItemList.map((item) => (
-              <div className="sales-order-items-row" key={item.id}>
-                <div className="sales-order-item-details">
-                  <input type="text" placeholder="Select an Item" />
+              <>
+                <div className="sales-order-items-row" key={item.id}>
+                  <div className="sales-order-item-details focus-outline">
+                    <input type="text" placeholder="Select an Item" />
+                  </div>
+                  <div className="sales-order-item-qty focus-outline">
+                    <input type="text" placeholder="1.00" />
+                  </div>
+                  <div className="sales-order-item-rate focus-outline">
+                    <input type="text" placeholder="0.00" />
+                  </div>
+                  <div className="sales-order-item-tax focus-outline">
+                    <input type="text" disabled />
+                  </div>
+                  <div className="sales-order-item-amount">
+                    <p>₹ 0.00</p>
+                  </div>
                 </div>
-                <div className="sales-order-item-qty">
-                  <input type="text" placeholder="1.00" />
-                </div>
-                <div className="sales-order-item-rate">
-                  <input type="text" placeholder="0.00" />
-                </div>
-                <div className="sales-order-item-tax">
-                  <input type="text" disabled />
-                </div>
-                <div className="sales-order-item-amount">
-                  <p>₹ 0.00</p>
-                </div>
-              </div>
+                <hr className="item-bottom-hr" />
+              </>
             ))}
-            <hr className="item-bottom-hr" />
           </div>
-          <p className="add-item-row-btn">+ Add Item</p>
+          <p className="add-item-row-btn" onClick={handleAddItemRow}>
+            + Add Item
+          </p>
+        </div>
+
+        <div className="attachments-sub-total-container">
+          <div className="attachments-container">
+            <div className="notes-input">
+              <p>Notes</p>
+              <input type="text" className="focus-outline" />
+            </div>
+            <div className="attachments-input">
+              <p>Attachments</p>
+              <Upload
+                action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                fileList={fileList}
+                onChange={onChange}
+              >
+                <Button className="upload-files-btn" icon={<img src="/images/icons/upload-files-icon.svg" />}>Upload Files</Button>
+              </Upload>
+            </div>
+          </div>
+
+          <div className="sub-total-container">
+            <div className="sub-total-row">
+              <p className="sub-total-heading">Sub Total</p>
+              <p className="sub-total-value">₹ 0.00</p>
+            </div>
+
+            <div className="discount-container">
+              <p className="add-discount-heading">Add Discount</p>
+            </div>
+
+            <div className="shiping-charge-container">
+              <p className="add-shiping-charge-heading">Add Shipping Charge</p>
+            </div>
+
+            <div className="round-off-container">
+              <p className="add-round-off-heading">Round Off</p>
+              <p className="round-off-value">₹ 0.00</p>
+            </div>
+
+            <div className="total-container">
+              <p className="total-heading">Total</p>
+              <p className="total-value">₹ 0.00</p>
+            </div>
+          
+          </div>
+              
         </div>
       </div>
+
+      <div className="terms-and-conditions-container">
+        <p className="terms-and-conditions-heading">Terms & Conditions</p>
+        <div className="terms-and-condition-text">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt, blanditiis. Dolore amet quia quae ipsam tenetur. Modi deserunt est, iste mollitia officiis tempore tenetur sapiente excepturi quos possimus, adipisci aut!
+            </p>
+        </div>
+      </div>
+      
     </div>
   );
 };
